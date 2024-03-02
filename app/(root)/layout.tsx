415434181611ce356555a597c29430fd240384fc
unknown <amrebrahem226@gmail.com>
2024-03-02 03:43:24 +0200

    Footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,12 +7,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        <Header />
-        <main className="flex-1">{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
   );
 }
