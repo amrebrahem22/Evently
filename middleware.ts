e2a4d2083ecda2894cc1678b7b6e7c8814d03436
unknown <amrebrahem226@gmail.com>
2024-03-10 12:39:58 +0200

    update middleware ignore path

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  // Routes that can be accessed while signed out
   publicRoutes: [
     "/",
     "/events/:id",
@@ -9,18 +8,13 @@ export default authMiddleware({
     "/api/webhook/stripe",
     "/api/uploadthing",
   ],
-  // Routes that can always be accessed, and have
-  // no authentication information
   ignoredRoutes: [
-    "/api/webhooks/clerk",
+    "/api/webhook/clerk",
     "/api/webhook/stripe",
     "/api/uploadthing",
   ],
 });
 
 export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
-  // for more information about configuring your Middleware
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
